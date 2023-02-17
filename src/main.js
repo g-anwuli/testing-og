a1dea718e9592682c8bcad2d9ef64d7331cb69e3
Godswill <godswillanwuli@gmail.com>
2023-02-17 04:53:26 +0100

    testing og 001

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import VueSocialSharing from "vue-social-sharing";
+import {VueMeta} from "vue-meta";
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(VueMeta).use(VueSocialSharing).mount("#app");
