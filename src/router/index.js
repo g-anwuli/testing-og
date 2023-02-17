0d2d3ee317559e7c334bf1f0bc0b828134659363
Godswill <godswillanwuli@gmail.com>
2023-02-17 13:52:04 +0100

    010 og

--- src/router/index.js
+++ src/router/index.js
@@ -61,13 +61,8 @@ const routes = [
         },
         {
           property: "og:url",
-          content: "https://godswilltest.netlify.app/about",
+          content: "https://godswilltest.netlify.app",
         },
-        // {
-        //   property: "og:image",
-        //   content:
-        //     "https://storage.googleapis.com/cryptofundme_dev_bucket/cryptofundme_media/creators/8/campaign_images/image_1_16647442114.jpg",
-        // },
       ],
     },
   },
@@ -79,15 +74,11 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  // This goes through the matched routes from last to first, finding the closest route with a title.
-  // e.g., if we have `/some/deep/nested/route` and `/some`, `/deep`, and `/nested` have titles,
-  // `/nested`'s will be chosen.
   const nearestWithTitle = to.matched
     .slice()
     .reverse()
     .find((r) => r.meta && r.meta.title);
 
-  // Find the nearest route element with meta tags.
   const nearestWithMeta = to.matched
     .slice()
     .reverse()
@@ -97,25 +88,19 @@ router.beforeEach((to, from, next) => {
     .slice()
     .reverse()
     .find((r) => r.meta && r.meta.metaTags);
-  // console.log(nearestWithMeta);
-  // console.log(nearestWithTitle);
-  // console.log("previus meta",previousNearestWithMeta);
-  // If a route with a title was found, set the document (page) title to that value.
+
   if (nearestWithTitle) {
     document.title = nearestWithTitle.meta.title;
   } else if (previousNearestWithMeta) {
     document.title = previousNearestWithMeta.meta.title;
   }
 
-  // Remove any stale meta tags from the document using the key attribute we set below.
   Array.from(document.querySelectorAll("[data-vue-router-controlled]")).map(
     (el) => el.parentNode.removeChild(el)
   );
 
-  // Skip rendering meta tags if there are none.
   if (!nearestWithMeta) return next();
 
-  // Turn the meta tag definitions into actual elements in the head.
   nearestWithMeta.meta.metaTags
     .map((tagDef) => {
       const tag = document.createElement("meta");
@@ -124,11 +109,9 @@ router.beforeEach((to, from, next) => {
         tag.setAttribute(key, tagDef[key]);
       });
 
-      // We use this to track which meta tags we create so we don't interfere with other ones.
       tag.setAttribute("data-vue-router-controlled", "");
       return tag;
     })
-    // Add the meta tags to the document head.
     .forEach((tag) => document.head.appendChild(tag));
 
   next();
