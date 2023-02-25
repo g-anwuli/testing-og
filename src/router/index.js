c432b0b58379e2c23e3957944b4d8f4634ef3885
Godswill <godswillanwuli@gmail.com>
2023-02-25 11:54:08 +0100

    new-fix

--- src/router/index.js
+++ src/router/index.js
@@ -65,7 +65,7 @@ const routes = [
         },
         {
           property: "og:url",
-          content: "https://godswilltest.netlify.app",
+          content: window.location.href,
         },
       ],
     },
