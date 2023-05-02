cb5b23f293acc03e1435498cb862aefbe7061ca2
Godswill <godswillanwuli@gmail.com>
2023-05-02 09:23:45 +0100

    notification

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <button @click="sendNotification">Send Notification</button>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'HomeView'
-}
+  name: "HomeView",
+  methods: {
+    requestPermission() {
+      if (!("Notification" in window)) {
+        throw new Error("Browser doesn't support Notification");
+      }
+      Notification.requestPermission().then((permission) =>
+        console.log(permission)
+      );
+    },
+    sendNotification() {
+      new Notification("Test", {
+        body: "This is a test Notification",
+        icon: "../assets/logo.png",
+      });
+    },
+  },
+  mounted() {
+    requestPermission();
+  },
+};
 </script>
