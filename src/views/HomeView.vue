51bfc4213b85a697bd55b2befd5520102e16f23a
Godswill <godswillanwuli@gmail.com>
2023-05-04 02:52:19 +0100

    fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,47 +9,47 @@
 export default {
   name: "HomeView",
   methods: {
-check(){
-if (!('serviceWorker' in navigator)) {
-throw new Error('No Service Worker support!')
-}
-if (!('PushManager' in window)) {
-throw new Error('No Push API Support!')
-}
-},
-async registerServiceWorker(){
-const swRegistration = await navigator.serviceWorker.register('sw.js'); //notice the file name
-return swRegistration;
-},
-async requestNotificationPermission(){
-const permission = await window.Notification.requestPermission();
-if(permission !== 'granted'){
-throw new Error('Permission not granted for Notification');
-}
-console.log(permission);
-},
+    check() {
+      if (!("serviceWorker" in navigator)) {
+        throw new Error("No Service Worker support!");
+      }
+      if (!("PushManager" in window)) {
+        throw new Error("No Push API Support!");
+      }
+    },
+    async registerServiceWorker() {
+      const swRegistration = await navigator.serviceWorker.register("sw.js"); //notice the file name
+      return swRegistration;
+    },
+    async requestNotificationPermission() {
+      const permission = await window.Notification.requestPermission();
+      if (permission !== "granted") {
+        throw new Error("Permission not granted for Notification");
+      }
+      console.log(permission);
+    },
     async sendNotification() {
-this.check()
-const swRegistration = await this.registerServiceWorker();
-const permission = await this.requestNotificationPermission();
+      this.check();
+      const swRegistration = await this.registerServiceWorker();
+      const permission = await this.requestNotificationPermission();
+      if (permission === "granted") {
         swRegistration.showNotification("Notification title", {
           icon: "http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png",
           body: "Hey there! You've been notified!",
+          vibrate: [200, 100, 200, 100, 200, 100, 200],
         });
+      }
     },
   },
-  mounted(){
-//Notification.requestPermission();
-}
 };
 </script>
 <style>
-button{
-background:blue;
-color:#fff;
-width:200px;
-height:50px;
-border:none;
-outline:none;
+button {
+  background: blue;
+  color: #fff;
+  width: 200px;
+  height: 50px;
+  border: none;
+  outline: none;
 }
 </style>
